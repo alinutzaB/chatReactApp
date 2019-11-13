18bbf304e4da5bddc339e13341b2d7075cca887d
alinutzaB <alinutza.ms@gmail.com>
2019-11-13 16:22:05 +0200

    Updates

---
+++ precache-manifest.4d5a2d7b0be3df655fafd5519fc76a39.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064bbd0230f9893ae1136b2407c681eb",
+    "url": "/chatReactApp/index.html"
+  },
+  {
+    "revision": "f53867fbb3ff2c26da46",
+    "url": "/chatReactApp/static/js/2.943564bc.chunk.js"
+  },
+  {
+    "revision": "25d6a2258b7a85b4de00",
+    "url": "/chatReactApp/static/js/main.cba499d8.chunk.js"
+  },
+  {
+    "revision": "c918f9f885a5a215d358",
+    "url": "/chatReactApp/static/js/runtime-main.d80a553d.js"
+  }
+]);
