36b7efe8f54bbbdffcc98534455534408850d0d3
alinutzaB <alinutza.ms@gmail.com>
2019-11-13 16:02:04 +0200

    Chat React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client'
+import Chatkit from '@pusher/chatkit-client'
+import MessageList from './MessageList'
+import Message from './Message'
+import SendMessageForm from './SendMessageForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { tokenUrl, instanceLocator } from './config'
+
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      messages: []
+    }
+
+    this.sendMessage = this.sendMessage.bind(this)
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+        instanceLocator,
+        userId: 'perborgen',
+        tokenProvider: new Chatkit.TokenProvider({
+            url: tokenUrl
+        })
+    })
+    
+    chatManager.connect()
+    .then(currentUser => {
+      this.currentUser = currentUser
+      this.currentUser.subscribeToRoom({
+        roomId: "517c600d-c933-4d61-a4f5-86e5d2ab9921",
+        hooks: {
+          onMessage: message => {
+            //console.log('message.text: ', message.text);
+            this.setState({
+              messages: [...this.state.messages, message]
+            })
+          }
+        },
+        messageLimit: 10
+      })
+    })
+  }
+
+  sendMessage(text) {
+    this.currentUser.sendMessage({
+      text,
+      roomId: "517c600d-c933-4d61-a4f5-86e5d2ab9921"
+    })
+  }
+
+  render() {
+    //console.log('this.state.messages:', this.state.messages);
+    return(
+      <div className="app">
+          <MessageList messages={this.state.messages} />
+          <SendMessageForm sendMessage={this.sendMessage}/>
+       </div>
+    );
+  }
 }
 
-export default App;
+
+
+export default App
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/252e56ff-2ed0-4fd5-9fd9-601be1f8f76e/token";
+const instanceLocator = "v1:us1:252e56ff-2ed0-4fd5-9fd9-601be1f8f76e";
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
